9110d88e077d15ebd61f9e8f0b1ef0022ad89731
jeon950523 <jeon950523@gmail.com>
2026-06-08 15:55:39 +0900

    파일폴더이동

--- msa4-meerkatgram-master/src/main/java/com/msa4meerkatgram/global/errors/custom/DeletedRecordException.java
+++
@@ -1,7 +0,0 @@
-package com.msa4meerkatgram.global.errors.custom;
-
-public class DeletedRecordException extends RuntimeException{
-    public DeletedRecordException(String message){
-        super(message);
-    }
-}
