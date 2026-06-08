8756c3b635fbc244ba3d7c771bc3aee849816e50
jeon950523 <jeon950523@gmail.com>
2026-06-08 13:09:45 +0900

    Initial commit

---
+++ msa4-meerkatgram-master/src/main/java/com/msa4meerkatgram/Msa4MeerkatgramApplication.java
@@ -0,0 +1,15 @@
+package com.msa4meerkatgram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+
+@ConfigurationPropertiesScan 
+@SpringBootApplication
+public class Msa4MeerkatgramApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Msa4MeerkatgramApplication.class, args);
+    }
+
+}
---
+++ msa4-meerkatgram-master/src/main/java/com/msa4meerkatgram/domain/auth/responses/AuthRes.java
@@ -0,0 +1,14 @@
+package com.msa4meerkatgram.domain.auth.responses;
+
+import com.msa4meerkatgram.domain.user.responses.UserRes;
+import lombok.Builder;
+
+@Builder
+public record AuthRes(
+    UserRes user
+    ,String accessToken
+) {
+    
+    
+    
+}
